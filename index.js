0bdb239368fb5965620d060e3db0392fb30cb519
Adrian Karalus <adrian@karalus.eu>
2023-06-12 00:41:32 +0200

    fix start button

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-var timeLeft = new Date(0);
 onReset();
+
+var everySecondInterval;
+var timeLeft = 0; // in ms
 
 function onReset() {
   onPause();
-  timeLeft = new Date(0);
-  timeLeft.setSeconds(15);
-  updateView();
+  timeLeft = 0;
+  resetView();
 }
 
-var everySecondInterval;
+function getPomodoroEndDateTime() {
+  let end = new Date(Date.now());
+  if (timeLeft > 0) {
+    end.setMilliseconds(timeLeft);
+  } else {
+    end.setMinutes(end.getMinutes() + 25);
+  }
+  return end;
+}
+
 function onStart() {
   onPause();
+  let endDateTime = getPomodoroEndDateTime();
   everySecondInterval = setInterval(() => {
-    if (timeLeft.getSeconds() <= 0) {
+    timeLeft = endDateTime.getTime() - Date.now();
+    if (timeLeft <= 0) {
       onPause();
+      timeLeft = 0;
       return;
     }
-    timeLeft.setSeconds(timeLeft.getSeconds() - 1);
     updateView();
-  }, 1000);
+  }, 250);
 }
 function onPause() {
   clearInterval(everySecondInterval);
 }
 
+function resetView() {
+  if (document.getElementById("timer") == null) return;
+  document.getElementById("timer").innerText = "25:00";
+}
+
 function updateView() {
-  if (document.getElementById("timer") != null) {
-    let minutesStr = ("00" + timeLeft.getMinutes()).slice(-2);
-    let secondsStr = ("00" + timeLeft.getSeconds()).slice(-2);
-    let timerString = minutesStr + ":" + secondsStr;
-    document.getElementById("timer").innerText = timerString;
-  }
+  if (document.getElementById("timer") == null) return;
+
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  let minutesStr = ("00" + minutes).slice(-2);
+  let secondsStr = ("00" + seconds).slice(-2);
+  let timerString = minutesStr + ":" + secondsStr;
+
+  document.getElementById("timer").innerText = timerString;
 }
